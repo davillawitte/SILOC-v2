077546e3808f1b302af78892c6c7361518ce5853
Marlon Secundo <marlon_secundo@outlook.com>
2022-07-02 10:37:52 -0300

    feat: nav element

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,36 +1,55 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHref, useLocation } from "react-router-dom";
 
 import "./index.css";
 
 function Header({ onSearchInput }) {
-  return (
-    <header className="cabecalho">
-      <h1>SILOC</h1>
-      <p>
-        Sistema Integrado de
-        <br />
-        Localização Cadavérica
-      </p>
+  const { pathname } = useLocation();
 
+  return (
+    <header className="siloc-header">
       <Link to="/SILOC-v2">
         <h1 className="logo"> SILOC </h1>
       </Link>
 
-      {onSearchInput && (
-        <div className="search-box">
-          <input
-            className="search-txt"
-            type="text"
-            name=""
-            onChange={(e) => {
-              onSearchInput(e.target.value);
-            }}
-            placeholder="Pesquisar cadáver pelo NIC"
-          />
-          <a className="search-btn" href="#"></a>
-        </div>
-      )}
+      <div className="siloc-title">
+        <h1>SILOC</h1>
+        <p>
+          Sistema Integrado de
+          <br />
+          Localização Cadavérica
+        </p>
+      </div>
+
+      <nav className="siloc-nav">
+        <Link to="/SILOC-v2">
+          <button className="nav-button">início</button>
+        </Link>
+
+        <div className="nav-space"></div>
+
+        {pathname !== "/dashboard" && (
+          <Link to="/dashboard">
+            <button className="nav-button">dashboard</button>
+          </Link>
+        )}
+
+        <div className="nav-space"></div>
+
+        {onSearchInput && (
+          <div className="search-box">
+            <input
+              className="search-txt"
+              type="text"
+              name=""
+              onChange={(e) => {
+                onSearchInput(e.target.value);
+              }}
+              placeholder="Pesquisar cadáver pelo NIC"
+            />
+          </div>
+        )}
+      </nav>
     </header>
   );
 }
